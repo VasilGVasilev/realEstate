6e799c92e1d270ebfd1f899ae6fb568e1a85bea0
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-05-17 18:57:29 +0300

    navbar fully functional
    mobile first responsive and toggle working

--- project/app/layout.js
+++ project/app/layout.js
@@ -1,6 +1,9 @@
+'use client'
+
 import './globals.css'
 import Link from 'next/link'
 import Image from 'next/image'
+import { useState } from 'react'
 
 export const metadata = {
   title: 'Ixora Residence by Creative Studio',
@@ -8,10 +11,17 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
+  const [toggleClicked, setToggleClick] = useState(false); 
+
+  const toggleNavbar = () => {
+    setToggleClick(state => !state)
+  }
+
   return (
     <html lang="bg">
       <body>
-        <a className='absolute top-0 left-6 h-16 w-16 flex justify-center bg-orange-400 border-x-2 border-white md:h-28 md:w-24 lg:h-36' href={`tel:00359886849995`}>
+        {/* persisting easy call button */}
+        <a className='absolute top-0 left-6 h-16 w-16 flex justify-center bg-orange-400 border-x-2 border-green-500 md:h-28 md:w-24 lg:h-36' href={`tel:00359886849995`}>
           <Image
             src="/green-phone.svg"
             alt="Phone Ring"
@@ -21,16 +31,40 @@ export default function RootLayout({ children }) {
             priority
           />
         </a>
-        <nav className='px-12 bg-gray-800 flex justify-end py-5 text-base'>
+        <div>
+
+          <nav className='px-12 flex justify-end py-5 text-base bg-gray-800 border-b-2 border-gray-500'>
+            
+            {/* desktop menu */}
               <div className='flex items-center text-white'>
-                <Link className='hidden p-3 lg:flex lg:p-6 lg:text-2xl md:flex md:p-3 md:text-xl hover:text-red-600 hover:bg-slate-700' href={'/'} >Начало</Link>
+                <Link className='hidden p-3 lg:flex lg:p-5 lg:text-2xl md:flex md:p-3 md:text-xl hover:text-red-600 hover:bg-slate-700' href={'/'} >Начало</Link>
                 <Link className='hidden p-3 lg:flex lg:p-5 lg:text-2xl md:flex md:p-3 md:text-xl hover:text-red-600 hover:bg-slate-700' href={'/description'}>Информация</Link>
                 <Link className='hidden p-3 lg:flex lg:p-5 lg:text-2xl md:flex md:p-3 md:text-xl hover:text-red-600 hover:bg-slate-700' href={'/apartments'}>Апартаменти</Link>
                 <Link className='hidden p-3 lg:flex lg:p-5 lg:text-2xl md:flex md:p-3 md:text-xl hover:text-red-600 hover:bg-slate-700' href={'/investor'}>Инвеститор</Link>
                 <Link className='hidden p-3 lg:flex lg:p-5 lg:text-2xl md:flex md:p-3 md:text-xl hover:text-red-600 hover:bg-slate-700' href={'/contacts'}>Контакти</Link>
-            </div>
-        </nav>
+              </div>
 
+            {/* mobile button */}
+            <div className='md:hidden flex items-center' onClick={toggleNavbar}>
+              <div className="space-y-2 cursor-pointer">
+                <span className="block w-8 h-0.5 bg-white"></span>
+                <span className="block w-8 h-0.5 bg-white"></span>
+                <span className="block w-8 h-0.5 bg-white"></span>
+              </div>
+            </div>
+
+          </nav>
+
+          {/* mobile menu */}
+          <div className={ toggleClicked === true ? 'md:hidden flex flex-col bg-gray-800 text-white' : 'hidden'}>
+            <Link className='flex justify-center p-3 text-base active:text-red-600 active:bg-slate-700' href={'/'} >Начало</Link>
+            <Link className='flex justify-center p-3 text-base active:text-red-600 active:bg-slate-700' href={'/description'}>Информация</Link>
+            <Link className='flex justify-center p-3 text-base active:text-red-600 active:bg-slate-700' href={'/apartments'}>Апартаменти</Link>
+            <Link className='flex justify-center p-3 text-base active:text-red-600 active:bg-slate-700' href={'/investor'}>Инвеститор</Link>
+            <Link className='flex justify-center p-3 text-base active:text-red-600 active:bg-slate-700' href={'/contacts'}>Контакти</Link>
+          </div>
+
+        </div>
         <main className='app'>
           <div className='py-32 text-center'>
             <h1 className='font-extrabold text-4xl'>End of RootLayout</h1>
