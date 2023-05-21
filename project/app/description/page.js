463d26f7364a23a06598eddcfec23d79a3594954
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-05-21 12:44:27 +0300

    edit text

--- project/app/description/page.js
+++ project/app/description/page.js
@@ -8,34 +8,34 @@ export default function Description() {
         {/* Container for List */}
         <div>
           
-          <div className="p-10 md:px-56 lg:px-96">
+          <div className="p-10 md:px-32 lg:px-40">
             <div className=" bg-white w-full h-fit ">
             
-              <ul className="p-10 border-2 border-orange-500">
+              <ul className="p-5 border-2 border-orange-500">
                 <div className="pb-7 bg-white text-orange-500 text-left text-2xl font-bold font-mono">За сградата</div>
-                <p className="text-orange-500 font-semibold">Фасадата</p>
-                <li className="text-gray-500"> Окачена, вентилируема, с каменна облицовка. </li>
+                <p className="sm:text-lg text-orange-500 font-semibold">Фасадата</p>
+                <li className="text-xs sm:text-sm text-gray-500 text-justify"> Окачена, вентилируема, с каменна облицовка. </li>
                 <br />
-                <p className="text-orange-500 font-semibold">Дограмата</p>
-                <li className="text-gray-500"> Най-висок клас Schüco.</li>
+                <p className="sm:text-lg text-orange-500 font-semibold">Дограмата</p>
+                <li className="text-xs sm:text-sm text-gray-500 text-justify"> Най-висок клас Schüco.</li>
                 <br />
-                <p className="text-orange-500 font-semibold">Асансиор</p>
-                <li className="text-gray-500">Безшумни електрически КОНЕ </li>
+                <p className="sm:text-lg text-orange-500 font-semibold">Асансиор</p>
+                <li className="text-xs sm:text-sm text-gray-500 text-justify">Безшумни електрически КОНЕ </li>
                 <br />
-                <p className="text-orange-500 font-semibold">Врати</p>
-                <li className="text-gray-500">Блиндирани входни врати SOLID, обърнати с мрамор.</li>
+                <p className="sm:text-lg text-orange-500 font-semibold">Врати</p>
+                <li className="text-xs sm:text-sm text-gray-500 text-justify">Блиндирани входни врати SOLID, обърнати с мрамор.</li>
                 <br />
-                <p className="text-orange-500 font-semibold">Отопление</p>
-                <li className="text-gray-500">Газово захранвано подово със самостоятелен термостат за всяко помещение на два от етажите, останалите два са с висок клас белгийски радиатори на Jaga, съобразявайки се с разнообразните предпочитания на клиентите. </li>
+                <p className="sm:text-lg text-orange-500 font-semibold">Отопление</p>
+                <li className="text-xs sm:text-sm text-gray-500 text-justify">Газово захранвано подово със самостоятелен термостат за всяко помещение на два от етажите, останалите два са с висок клас белгийски радиатори на Jaga, съобразявайки се с разнообразните предпочитания на клиентите. </li>
                 <br />
-                <p className="text-orange-500 font-semibold">Гаражи</p>
-                <li className="text-gray-500">Голям брой гаражи в сутерена и допълнителни паркоместа извън сградата. </li>
+                <p className="sm:text-lg text-orange-500 font-semibold">Гаражи</p>
+                <li className="text-xs sm:text-sm text-gray-500 text-justify">Голям брой гаражи в сутерена и допълнителни паркоместа извън сградата. </li>
                 <br />
-                <p className="text-orange-500 font-semibold">Сигурност</p>
-                <li className="text-gray-500">Ограда и контролиран достъп с чип и лицево разпознаване до двора и сградата. </li>
+                <p className="sm:text-lg text-orange-500 font-semibold">Сигурност</p>
+                <li className="text-xs sm:text-sm text-gray-500 text-justify">Ограда и контролиран достъп с чип и лицево разпознаване до двора и сградата. </li>
                 <br />
-                <p className="text-orange-500 font-semibold">Удобства</p>
-                <li className="text-gray-500">Самостоятелен фитнес и спа център с джакузи, парна баня и сауна, заедно с топли лежанки - напълно оборудвани за бъдещите обитатели. </li>
+                <p className="sm:text-lg text-orange-500 font-semibold">Удобства</p>
+                <li className="text-xs sm:text-sm text-gray-500 text-justify">Самостоятелен фитнес и спа център с джакузи, парна баня и сауна, заедно с топли лежанки - напълно оборудвани за бъдещите обитатели. </li>
               </ul>
             </div>
           </div>
@@ -50,7 +50,7 @@ export default function Description() {
           <iframe 
             src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Sofia,%20Bulgaria,%20Europe+(Ixora%20Luxury%20Residence)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" 
             width="100%" 
-            height="400" 
+            height="400"
           ></iframe>
         </div>
         {/* Container for Link to Building Plan */}
