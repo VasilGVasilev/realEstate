97c9bd2f14e02183abe81493ccbd57dbabe94a1e
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-05-21 15:29:20 +0300

    add

--- project/app/apartments/page.js
+++ project/app/apartments/page.js
@@ -5,7 +5,22 @@ export const metadata = {
 export default function Apartments() {
     return (
       <div>
-        
+        <div className="relative inline-block">
+          <div className="absolute firstFloor left-6 top-11 h-full w-full z-10 hover:bg-black hover:opacity-40">asdsa</div>
+          <div className="absolute firstFloor left-6 top-28 h-full w-full z-10 hover:bg-black hover:opacity-40">asdsa</div>
+
+          <img src="ixora-apartments.webp" alt="Ixora Plan" className="relative" />
+          {/* <div className="absolute top-0 z-10 cursor-pointer penthouse hover:bg-black hover:bg-opacity-50 text-white" ></div> */}
+          {/* <map name="building" className="">
+              <area className="" shape="rect" coords="10,10,100,100" alt="Computer" href="computer.htm" />
+            
+          </map> */}
+
+
+
+
+        </div>
+
       </div>
     )
 }
