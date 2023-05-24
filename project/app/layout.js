283135643f32ce09a18f59db958a644edb6ea238
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-05-24 12:05:28 +0300

    navbar ready sticky

--- project/app/layout.js
+++ project/app/layout.js
@@ -25,37 +25,58 @@ export default function RootLayout({ children }) {
       <body className='bg-gray-900'>
         <header>
 
-          <nav className='fixed z-20 px-12 py-5 flex space-x-60 text-base bg-gradient-to-b from-gray-900 to-gray-700 border-b-2 border-gray-500'>
+          <nav className='fixed z-20 flex-col w-full text-base bg-gradient-to-b from-gray-900 to-gray-700 border-b-2 border-gray-500'>
+            <div className='px-10 py-3 flex justify-between'>
+              <div>
 
-            <a className='absolute top-0 left-6 h-16 w-16 flex justify-center bg-gradient-to-b from-orange-500 to-orange-300 border-x-2 border-green-500 md:h-28 md:w-24 lg:h-36' href={`tel:00359886849995`}>
-              <Image
-                src="/green-phone.svg"
-                alt="Phone Ring"
-                className="bg-transparent animate-pulse"
-                width={45}
-                height={24}
-              />
-            </a>
-            
-            {/* desktop menu */}
-            <div className='flex items-center text-white' >
-              <Link className='hidden p-3 lg:flex lg:p-5 lg:text-xl md:flex md:p-3 md:text-base hover:text-red-600 hover:bg-slate-700' href={'/'} >Начало</Link>
-              <Link className='hidden p-3 lg:flex lg:p-5 lg:text-xl md:flex md:p-3 md:text-base hover:text-red-600 hover:bg-slate-700' href={'/description'}>За сградата</Link>
-              <Link className='hidden p-3 lg:flex lg:p-5 lg:text-xl md:flex md:p-3 md:text-base hover:text-red-600 hover:bg-slate-700' href={'/apartments'}>Апартаменти</Link>
-              <Link className='hidden p-3 lg:flex lg:p-5 lg:text-xl md:flex md:p-3 md:text-base hover:text-red-600 hover:bg-slate-700' href={'/investor'}>Инвеститор</Link>
-              <Link className='hidden p-3 lg:flex lg:p-5 lg:text-xl md:flex md:p-3 md:text-base hover:text-red-600 hover:bg-slate-700' href={'/contacts'}>Контакти</Link>
-            </div>
+                <a className='relative h-8 w-20 flex justify-center bg-gradient-to-b from-orange-500 to-orange-300 md:h-12 md:w-24 lg:h-20 lg:w-48 border-2 border-white md:border-0 md:hover:border-2 md:hover:border-white' href={`/`}>
+                  <Image
+                    src="/ixora.png"
+                    alt="Phone Ring"
+                    className=""
+                    height={1}
+                    width={300}
 
-            
+                  />
+                </a>
+              </div>
 
-            {/* mobile button */}
-            <div className='md:hidden flex items-center' onClick={toggleNavbar}>
-              <div className="space-y-2 cursor-pointer">
-                <span className="block w-8 h-0.5 bg-white"></span>
-                <span className="block w-8 h-0.5 bg-white"></span>
-                <span className="block w-8 h-0.5 bg-white"></span>
+              <div>
+
+                {/* desktop menu */}
+                <div className='flex items-center text-white' >
+                  <Link className='hidden p-3 lg:flex lg:p-5 lg:text-xl md:flex md:p-3 md:text-sm hover:text-red-600 hover:bg-slate-700' href={'/'} >Начало</Link>
+                  <Link className='hidden p-3 lg:flex lg:p-5 lg:text-xl md:flex md:p-3 md:text-sm hover:text-red-600 hover:bg-slate-700' href={'/description'}>За сградата</Link>
+                  <Link className='hidden p-3 lg:flex lg:p-5 lg:text-xl md:flex md:p-3 md:text-sm hover:text-red-600 hover:bg-slate-700' href={'/apartments'}>Апартаменти</Link>
+                  <Link className='hidden p-3 lg:flex lg:p-5 lg:text-xl md:flex md:p-3 md:text-sm hover:text-red-600 hover:bg-slate-700' href={'/investor'}>Инвеститор</Link>
+                  <Link className='hidden p-3 lg:flex lg:p-5 lg:text-xl md:flex md:p-3 md:text-sm hover:text-red-600 hover:bg-slate-700' href={'/contacts'}>Контакти</Link>
+                </div>
+
+                {/* mobile button */}
+                <div className='md:hidden flex items-center pt-1' onClick={toggleNavbar}>
+                  <div className="space-y-2 cursor-pointer">
+                    <span className="block w-8 h-0.5 bg-white"></span>
+                    <span className="block w-8 h-0.5 bg-white"></span>
+                    <span className="block w-8 h-0.5 bg-white"></span>
+                  </div>
+                </div>
+
               </div>
             </div>
+
+            <div className=''>
+
+              {/* mobile menu */}
+              <div onClick={toggleNavbar} className={ toggleClicked === true ? 'md:hidden flex flex-col bg-gray-800 w-full text-white' : 'hidden'}>
+                <Link className='flex justify-center p-3 text-base active:text-red-600 active:bg-slate- hover:text-red-600 hover:bg-slate-700' href={'/'} >Начало</Link>
+                <Link className='flex justify-center p-3 text-base active:text-red-600 active:bg-slate- hover:text-red-600 hover:bg-slate-700' href={'/description'}>За сградата</Link>
+                <Link className='flex justify-center p-3 text-base active:text-red-600 active:bg-slate- hover:text-red-600 hover:bg-slate-700' href={'/apartments'}>Апартаменти</Link>
+                <Link className='flex justify-center p-3 text-base active:text-red-600 active:bg-slate- hover:text-red-600 hover:bg-slate-700' href={'/investor'}>Инвеститор</Link>
+                <Link className='flex justify-center p-3 text-base active:text-red-600 active:bg-slate- hover:text-red-600 hover:bg-slate-700' href={'/contacts'}>Контакти</Link>
+              </div>
+
+            </div>
+            
 
           </nav>
 
