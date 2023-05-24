4d7e2b23e32ed48227aca6173414f3e3f8ac05fb
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-05-24 19:18:47 +0300

    total m2 for floor 1

--- project/app/apartments/first-floor/page.js
+++ project/app/apartments/first-floor/page.js
@@ -1,5 +1,7 @@
 'use client'
 
+import { useState } from "react";
+
 
 export const metadata = {
   title: 'Първи етаж - Ixora Residence',
@@ -11,8 +13,15 @@ const reserved = "hover:bg-blue-400 hover:opacity-40 active:bg-blue-400 active:o
 
 
 export default function firstFloor() {
+    const [apartment, setApartment] = useState(null);
 
+    const updateInfo = (currentAp) => {
+      setApartment(s=>currentAp);
+      if(window.innerWidth > 413){
+        window.scrollTo(0, 600);
+      }
 
+    }
 
     return (
       <div className="p-10 bg-white">
@@ -20,21 +29,107 @@ export default function firstFloor() {
           <div className="py-4 text-white text-center text-2xl font-extrabold">Първи етаж</div>
         </div>
 
-        <div className="relative inline-block">
-
-          <div className={`absolute apartmentFour h-full w-full z-10 ${sold}`}></div>
-          <div className={`absolute apartmentThree h-full w-full z-10 ${reserved}`} ></div>
-          <div className={`absolute apartmentTwo h-full w-full z-10 ${available} `} ></div>
-          <div className={`absolute apartmentOne h-full w-full z-10 ${available} `} ></div>
-          
-
-          <img src="/plans/first-floor.webp" alt="Ixora Plan" className="relative" />
-
-
-
+        <div className="pt-10 pb-10 flex-col text-orange-500">
+          {/* four static templates */}
+          {/* default */}
+          {apartment === null ? <div className=" font-bold text-lg text-center">Площообразуване</div> : null}
+          {/* 1 */}
+          {apartment === 1 ? <div className="border-r-2 border-l-2 border-orange-500 text-center" >
+            <div className="pb-4 text-lg font-bold">Площообразуване - ап. 1</div>
+            <div className="flex justify-center space-x-3 ">
+              <div>Площ</div>
+              <div className="text-black"></div>
+              <div className="font-bold">182,43 м²</div>
+            </div>
+            <div> + </div>
+            <div className="flex justify-center space-x-3 ">
+              <div>Двор</div>
+              <div className="text-black"></div>
+              <div className="font-bold">73,90 м²</div>
+            </div>
+            <div> = </div>
+            <div className="flex justify-center space-x-3 ">
+              <div>Обща площ</div>
+              <div className="text-black"></div>
+              <div className="font-bold">256,33 м²</div>
+            </div>
+          </div> : null}
+          {/* 2 */}
+          {apartment === 2 ? <div className="border-r-2 border-l-2 border-orange-500 text-center" >
+            <div className="pb-4 text-lg font-bold">Площообразуване - ап. 2</div>
+            <div className="flex justify-center space-x-3 ">
+              <div>Площ</div>
+              <div className="text-black"></div>
+              <div className="font-bold">93,02 м²</div>
+            </div>
+            <div> + </div>
+            <div className="flex justify-center space-x-3 ">
+              <div>Двор</div>
+              <div className="text-black"></div>
+              <div className="font-bold">69,00 м²</div>
+            </div>
+            <div> = </div>
+            <div className="flex justify-center space-x-3 ">
+              <div>Обща площ</div>
+              <div className="text-black"></div>
+              <div className="font-bold">162,02 м²</div>
+            </div>
+          </div> : null}
+          {/* 3 */}
+          {apartment === 3 ? <div className="border-r-2 border-l-2 border-orange-500 text-center" >
+            <div className="pb-4 text-lg font-bold">Площообразуване - ап. 3</div>
+            <div className="flex justify-center space-x-3 ">
+              <div>Площ</div>
+              <div className="text-black"></div>
+              <div className="font-bold">146,04 м²</div>
+            </div>
+            <div> + </div>
+            <div className="flex justify-center space-x-3 ">
+              <div>Двор</div>
+              <div className="text-black"></div>
+              <div className="font-bold">45,70 м²</div>
+            </div>
+            <div> = </div>
+            <div className="flex justify-center space-x-3 ">
+              <div>Обща площ</div>
+              <div className="text-black"></div>
+              <div className="font-bold">191,74 м²</div>
+            </div>
+          </div> : null}
+          {/* 4 */}
+          {apartment === 4 ? <div className="border-r-2 border-l-2 border-orange-500 text-center" >
+            <div className="pb-4 text-lg font-bold">Площообразуване - ап. 4</div>
+            <div className="flex justify-center space-x-3 ">
+              <div>Площ</div>
+              <div className="text-black"></div>
+              <div className="font-bold">82,96 м²</div>
+            </div>
+            <div> + </div>
+            <div className="flex justify-center space-x-3 ">
+              <div>Двор</div>
+              <div className="text-black"></div>
+              <div className="font-bold">22,20 м²</div>
+            </div>
+            <div> = </div>
+            <div className="flex justify-center space-x-3 ">
+              <div>Обща площ</div>
+              <div className="text-black"></div>
+              <div className="font-bold">105,16 м²</div>
+            </div>
+          </div> : null}
         </div>
 
 
+        <div className="relative inline-block">
+
+          <div className={`absolute apartmentFour h-full w-full z-10 ${sold}`} onClick={()=>updateInfo(4)}></div>
+          <div className={`absolute apartmentThree h-full w-full z-10 ${reserved}`} onClick={()=>updateInfo(3)}></div>
+          <div className={`absolute apartmentTwo h-full w-full z-10 ${available} `} onClick={()=>updateInfo(2)}></div>
+          <div className={`absolute apartmentOne h-full w-full z-10 ${available} `} onClick={()=>updateInfo(1)}></div>
+          
+          <img src="/plans/first-floor.webp" alt="Ixora Plan" className="relative" />
+
+        </div>
 
 
         <div className="py-7">
