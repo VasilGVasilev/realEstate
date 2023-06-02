1e2485d802924b7f47526724b6bbe4a2fa88e018
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-06-02 19:47:22 +0300

    imgloader and sitemap to ixorabg

--- project/app/imgLoader.js
+++ project/app/imgLoader.js
@@ -1,5 +1,5 @@
 export const imageLoader = ({ src, width, quality }) => {
-    return `https://testvgv.net${src}?w=${width}&q=${quality || 75}`;
+    return `https://ixorabg.com${src}?w=${width}&q=${quality || 75}`;
 };
 
 // Crucial for Image Component loading, which as a property 'loader' is not very explicit in the Next.js docs, used Phind AI for instructions
