11f8775867d9046d2ab8f4e0af72a8f94aef2315
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-06-16 17:29:48 +0300

    Update layout.js

--- ixora/app/layout.js
+++ ixora/app/layout.js
@@ -1,7 +1,6 @@
 'use client'
 
 import './globals.css'
-import Link from 'next/link'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import { imageLoader } from './imgLoader'
