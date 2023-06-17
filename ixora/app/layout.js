6890e65ae8598b3eec1b15df2ec93ee5b8bbd556
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-06-17 12:19:00 +0300

    Update layout.js

--- ixora/app/layout.js
+++ ixora/app/layout.js
@@ -32,7 +32,7 @@ export default function RootLayout({ children }) {
  
         {/* fixed navbar requires py same for all non home pages */}
         {pathName === '/' ? (<></>) : (
-        <div className='py-[7%] sm:py-[5%] md:py-[2.5%] bg-ixora-dark'></div>
+        <div className='py-[7%] sm:py-[5%] md:py-[3.5%] bg-ixora-dark'></div>
         )}
         <main className='app'>
           {children}
