27de41f73b312017c1fed3491ba5735ecf63472a
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-06-21 19:28:52 +0300

    welcome text update

--- ixora/app/Home.js
+++ ixora/app/Home.js
@@ -35,7 +35,7 @@ export default function Home() {
                     Your browser does not support the video tag.
                 </video>
 
-                <div className="absolute bottom-[10%] left-0 w-full h-full flex items-end justify-center ">
+                <div className="absolute bottom-[15%] left-0 w-full h-full flex items-end justify-center ">
                     <motion.div 
                         className="welcomeTextOnImg text-white text-center text-2xl md:text-4xl lg:text-6xl font-bold"
                         variants={container}
